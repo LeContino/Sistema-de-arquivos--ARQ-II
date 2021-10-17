f0534c6f9a3b486d062cb79906b62ed66c121d97
LeContino <lelecontino@hotmail.com>
2021-10-17 00:17:37 -0300

    Update leitura_de_arquivo.c
    escrita em arquivo binário das definições da FAT

--- Sistema_de_arquivos/leitura_de_arquivo.c
+++ Sistema_de_arquivos/leitura_de_arquivo.c
@@ -1,52 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define padrao 256      //padrao representa o 2^8 que aparece diversas vezes no enunciado do problema
-#define tam_cluster 32000 // 32kb
-#define zero 0
+#define padrao 256  //padrao representa o 2^8 que aparece diversas vezes no enunciado do problema
+//#define tam_cluster 32000  // 32kb
 
 int main(){
 
     FILE*fat;
+    int val=1;
     int tam_indice=padrao;
     int inicio_indice=0;
     int pos_cluster_um=0;       //posição do primeiro cluster
     int distancia=0;            //calcula o tamanho total dos indices , ou seja, (posição inicial + 255)*sizeof(int)
     int i,j;
-    int fim_linha=0;
+    int tam_cluster = 32000;
 
 //criação do arquivo
 
-    if(!(fat =fopen ("arquivo.txt","wb"))) //abertura do arquivo
+    if(!(fat =fopen ("arquivo1.bin","wb"))) //abertura do arquivo
         printf("Erro na abertura do arquivo \n");
 
     else
     {
-        fprintf(fat,"%d",tam_indice);   //escreve tam do indice
-        fprintf(fat,"%d",tam_cluster);  //escreve tam do cluster
+        fwrite(&tam_indice,sizeof(int),1,fat);   //escreve tam do indice
+        fwrite(&tam_cluster,sizeof(int),1,fat);  //escreve tam do cluster
         fseek(fat,4*sizeof(int),SEEK_SET); // pula tam_indice,tam_cluster,inicio_indice,inicio_root
         inicio_indice=ftell(fat);           //posição do inicio_indice
         fseek(fat,2*sizeof(int),SEEK_SET); // volta para inicio_indice para escrever a posição
-        fprintf(fat,"%d",inicio_indice); //escreve onde inicia o indice
+        fwrite(&inicio_indice,sizeof(int),1,fat); //escreve onde inicia o indice
         distancia=(inicio_indice+padrao-1)*sizeof(int); //16+256-1 *8
         fseek(fat,distancia,SEEK_SET); //inicio_ind +256-1
         pos_cluster_um=ftell(fat);
         fseek(fat,3*sizeof(int),SEEK_SET); //retorna ´para a posição de metalinguagem inicio_root
-        fprintf(fat,"%d \n",pos_cluster_um);  //escreve posição do 1°cluster no arquivo ->inicio_root  e passa para a proxima linha
+        fwrite(&pos_cluster_um,sizeof(int),1,fat);  //escreve posição do 1°cluster no arquivo ->inicio_root  e passa para a proxima linha
         fseek(fat,pos_cluster_um,SEEK_SET); //acha 1°cluter
 
         for (i=0;i<padrao;i++){     //qntidade de clusters
             for (j=0;j<tam_cluster;j++) // cada cluster
             {
-                if(j%padrao==0) // segue tamanho dos indices
-                {
-                    fprintf(fat,"/n"); // passa para a proxima linha
-                }
-                fprintf(fat,"%d",zero); //printa o valor zero de todos os clusters para determinar o tamanho máx do arquivo
+                fwrite(&val,sizeof(int),1,fat); //printa o valor zero de todos os clusters para determinar o tamanho máx do arquivo
             }
         }
     }
     fclose(fat); // fecha arquivo
-
 
 }
