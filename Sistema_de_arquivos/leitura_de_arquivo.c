578a27ac59a7f5ec42a055a0c52a6bdd1009ad6c
Gabriel Madeira <Gabriel Madeira>
2021-10-21 23:01:35 +0100

    Pair programming #2

--- Sistema_de_arquivos/leitura_de_arquivo.c
+++ Sistema_de_arquivos/leitura_de_arquivo.c
@@ -21,12 +21,12 @@ typedef struct file_folder_item_pattern
     short ponteiro;
     char nome[16];
 
-} FFIPATTERN;
+} ITEM;
 
 typedef struct metadados
 {
 
-    char tam_indice;            // char e implicitamente transformado em um numero  --> 8 bits
+    short tam_indice;           // char e implicitamente transformado em um numero  --> 16 bits
     unsigned short tam_cluster; // 1kB= 1024B
     short indiceStartice;       //inicio do indice --> 16 bits
     short inicio_root;          //posicao do primeiro cluster
@@ -35,26 +35,26 @@ typedef struct metadados
 
 typedef struct
 {
-    FFIPATTERN *array;
+    ITEM *array;
     size_t used;
     size_t size;
 } Array;
 
 void initArray(Array *a)
 {
-    a->array = malloc(sizeof(FFIPATTERN));
+    a->array = malloc(sizeof(ITEM));
     a->used = 0;
-    a->size = sizeof(FFIPATTERN);
+    a->size = sizeof(ITEM);
 }
 
-void insertArray(Array *a, FFIPATTERN element)
+void insertArray(Array *a, ITEM element)
 {
     // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
     // Therefore a->used can go up to a->size
     if (a->used == a->size)
     {
         a->size *= 2;
-        a->array = realloc(a->array, a->size * sizeof(FFIPATTERN));
+        a->array = realloc(a->array, a->size * sizeof(ITEM));
     }
     a->array[a->used++] = element;
 }
@@ -86,8 +86,9 @@ void cria_arquivo(METADADOS dados, FILE *arq)
     else
     {
         fwrite(&dados, sizeof(METADADOS), 1, arq); // printa os valores de metadado que sao: 256 (de 0 a 255) , 4,0, 32768
-
-        for (i = 0; i < pattern - 1; i++)
+        // int numberOfClusters = pattern;
+        int numberOfClusters = 1;
+        for (i = 0; i < numberOfClusters; i++)
         {                                     //qntidade de clusters
             for (j = 0; j < clusterSize; j++) // cada cluster
             {
@@ -124,44 +125,60 @@ void le_metadados(FILE *arq)
 // - Cluster de continuação de pastas
 // - Cluster de continuação de arquivo
 
-Array readFolderContent(FILE *arq)
+ITEM *readFolderContent(FILE *arq)
 {
     //0 - 1101010100101101001
     //hF    p/a    end1  end2        ( Nome )
     // fread (void *buffer, int numero_de_bytes, int count, FILE *fp);
 
-    Array arr;
-    FFIPATTERN *item;
-    initArray(&arr);
-    int i = 0;
+    ITEM arr[256];
+    ITEM *i;
+    bool t = 1;
+    int tt = 0;
 
-    while (1)
+    //printf("posição chegada: %d \n", ftell(arq));
+    while (t)
     {
-        fread(item, sizeof(FFIPATTERN), 1, arq);
-        if (!(*item).ler_bit || (i > clusterSize / sizeof(FFIPATTERN)))
-            break;
+        fseek(arq, 1, SEEK_CUR);
+        printf("%i\n", tt);
+        tt += 1;
+        printf("%d\n", ftell(arq));
+        fread(i, sizeof(ITEM), 1, arq);
+        printf('%s\n', i->nome);
+        //printf("ler_bit: %d \n", i->ler_bit);
+        //printf("posição apos leitura: %d \n", ftell(arq));
+        if (!(i->ler_bit) /*|| (i > clusterSize / sizeof(ITEM))*/)
+        {
+            printf("acabou os itens\n");
+            t = 0;
+            fseek(arq, -(sizeof(ITEM)), SEEK_CUR);
+        }
 
         else
         {
-            insertArray(&arr, (*item));
+            printf('%s\n', i->nome);
+            arr[0].eh_pasta_bit = i->eh_pasta_bit;
+            arr[0].ler_bit = i->ler_bit;
+            strcpy(arr[0].nome, i->nome);
+            arr[0].ponteiro = i->ponteiro;
         }
+    }
 
-        i += 1;
-    };
-
-    return arr;
+    return &arr;
 }
 
 void goToCluster(FILE *arq, int n)
 {
-    int metadataDesloc = 16 + 16 + 16 + 8 - 1;
-    int fatTableDesloc = pattern * 8;
-    int clusterDesloc = n * clusterSize;
+    int metadataDesloc = sizeof(METADADOS) - 1;
+    int fatTableDesloc = pattern;
+    int clusterDesloc = (n * clusterSize) / 8;
     int desloc = metadataDesloc + fatTableDesloc + clusterDesloc;
-    fseek(arq, desloc, SEEK_SET);
+    printf("desloc: %d \n", desloc);
+    fseek(arq, desloc, SEEK_SET); //deslocamento medido em bytes
+    printf("going to cluster %d \n", n);
 }
 
-void writeItemInFolderCluster(FILE *arq, int cluster, FFIPATTERN item)
+void writeItemInFolderCluster(FILE *arq, int cluster, ITEM item)
 {
     goToCluster(arq, cluster);
     bool *i;
@@ -172,22 +189,33 @@ void writeItemInFolderCluster(FILE *arq, int cluster, FFIPATTERN item)
         fseek(arq, (7 * sizeof(bool)), SEEK_CUR);
     }
 
-    fwrite(&item, sizeof(FFIPATTERN), 1, arq);
+    fwrite(&item, sizeof(ITEM), 1, arq);
 }
 
-void writeItemInCurrFolder(FILE *arq, FFIPATTERN *item)
+void writeItemInCurrFolder(FILE *arq, ITEM *item)
 {
-    bool *i;
+    ITEM *i;
+    bool t = 1;
 
-    while (i)
+    //printf("posição chegada: %d \n", ftell(arq));
+    while (t)
     {
-        fread(i, sizeof(bool), 1, arq);
-        fseek(arq, (7 * sizeof(bool)), SEEK_CUR);
+        fread(i, sizeof(ITEM), 1, arq);
+        //printf("ler_bit: %d \n", i->ler_bit);
+        //printf("posição apos leitura: %d \n", ftell(arq));
+        if (!(i->ler_bit) /*|| (i > clusterSize / sizeof(ITEM))*/)
+        {
+            t = 0;
+            fseek(arq, -(sizeof(ITEM)), SEEK_CUR);
+        }
     }
 
-    (*item).ponteiro = 11; // navegar a fat até achar local para criar a pasta/item
+    //printf("posição recuo: %d \n", ftell(arq));
+    //(*item).ponteiro = 11; // navegar a fat até achar local para criar a pasta/item
 
-    fwrite(&item, sizeof(FFIPATTERN), 1, arq);
+    fwrite(&item, sizeof(ITEM), 1, arq);
+
+    //printf("posição pos escrita: %d \n", ftell(arq));
 }
 
 void CD(FILE *arq, char *comando)
@@ -200,42 +228,42 @@ void DIR(FILE *arq)
 
 void RM() {}
 
-void MKDIR(FILE *arq, char *name)
-{
-    FFIPATTERN temp;
+// void MKDIR(FILE *arq, char *name)
+// {
+//     ITEM *temp;
 
-    temp.ler_bit = 1;
-    temp.eh_pasta_bit = 1;
-    strcpy(&temp.nome, name);
+//     (*temp).ler_bit = 1;
+//     (*temp).eh_pasta_bit = 1;
+//     strcpy(temp.nome, name);
 
-    writeItemInCurrFolder(arq, &temp);
-}
+//     writeItemInCurrFolder(arq, &temp);
+// }
 
 void MKFILE() {}
 
-void RENAME(FILE *arq, char *name, char *newname)
-{
-    int i = 0;
-    FFIPATTERN item;
+// void RENAME(FILE *arq, char *name, char *newname)
+// {
+//     int i = 0;
+//     ITEM item;
 
-    while (1)
-    {
-        fread(&item, sizeof(FFIPATTERN), 1, arq);
-        if (item.ler_bit || (i > clusterSize / sizeof(FFIPATTERN)))
-            break;
+//     while (1)
+//     {
+//         fread(&item, sizeof(ITEM), 1, arq);
+//         if (item.ler_bit || (i > clusterSize / sizeof(ITEM)))
+//             break;
 
-        else
-        {
-            if (strcmp(&item.nome, name))
-            {
-                strcpy(&item.nome, newname);
-                fseek(arq, -(sizeof(FFIPATTERN)), SEEK_CUR);
-                fwrite(&item, sizeof(FFIPATTERN), 1, arq);
-            }
-        }
-        i += 1;
-    }
-}
+//         else
+//         {
+//             if (strcmp(&item.nome, name))
+//             {
+//                 strcpy(&item.nome, newname);
+//                 fseek(arq, -(sizeof(ITEM)), SEEK_CUR);
+//                 fwrite(&item, sizeof(ITEM), 1, arq);
+//             }
+//         }
+//         i += 1;
+//     }
+// }
 
 void MOVE() {}
 
@@ -257,8 +285,35 @@ int main()
     FILE *arq;
     METADADOS dados = {pattern, clusterSize, indiceStart, rootPos};
 
+    cria_arquivo(dados, arq);
+
+    arq = fopen("arquivo.bin", "r+");
+
+    printf("#1\n");
+    goToCluster(arq, 0);
+
+    printf("#2\n");
+    ITEM *item;
+    item = malloc(sizeof(ITEM));
+    item->ler_bit = true;
+    item->eh_pasta_bit = true;
+    item->ponteiro = 255;
+    strcpy(item->nome, "Item1");
+    writeItemInCurrFolder(arq, item);
+
+    printf("#3\n");
+    goToCluster(arq, 0);
+    printf("#4\n");
+    ITEM *folderItems = readFolderContent(arq);
+    printf("#5\n");
+    printf("nome: %s \n", (folderItems[0]).nome);
+
+    fclose(arq);
+
+    // [ 1010000000000000000000000010 ]
+    // se for pasta na hora de criar item no diretorio atual -> entrar no cluster da pasta e inicializar pasta (., ..)
+
     //interfaceLoop();
 
-    //cria_arquivo(dados, arq);
     //le_metadados(arq);
 }
