3b09f9d040cac7ec191cc122ff642821b149a03b
Gabriel Madeira <Gabriel Madeira>
2021-10-24 23:06:07 +0100

    Pair programming #5

--- Sistema_de_arquivos/leitura_de_arquivo.c
+++ Sistema_de_arquivos/leitura_de_arquivo.c
@@ -10,8 +10,10 @@
 //SEEK_SET       0        Início do arquivo
 //SEEK_CUR       1        Ponto corrente no arquivo
 //SEEK_END       2        Fim do arquivo
+#define maxCharsInFileSystem 1024000
 
 // int fseek (FILE *fp,long numbytes,int origem);
+int currentFolderCluster = 0;
 
 typedef struct file_folder_item_pattern
 {
@@ -38,17 +40,35 @@ typedef struct clmeta
     int pontoponto;
 } clmeta;
 
-readFat(int cluster){
+short readFat(FILE *arq, int cluster)
+{
+    short ponteiro;
 
+    fseek(arq, ((cluster + 9) * sizeof(char)), SEEK_SET); // va para byte 9 (inicio do indice +1 (pula o zero))+posicao
+    fread(&ponteiro, sizeof(short), 1, arq);
+
+    return ponteiro;
 };
-void writeFat(int cluster, char content){
 
+void writeFat(FILE *arq, int cluster, char content)
+{
+
+    fseek(arq, ((cluster + 9) * sizeof(char)), SEEK_SET); // va para byte 9 (inicio do indice +1 (pula o zero))+posicao
+    fwrite(&content, sizeof(char), 1, arq);
+};
+
+void apagaCluster(FILE *arq) // Apaga o cluster atual
+{
+    int i, val = 0;
+    for (i = 0; i < clusterSize / 8; i++)
+    {
+        fwrite(&val, sizeof(char), 1, arq);
+    }
 };
 
 void cria_arquivo(METADADOS dados, FILE *arq)
 {
-    int i, j;
-    char val = 0;
+    int i;
 
     //criacao do arquivo
 
@@ -61,11 +81,8 @@ void cria_arquivo(METADADOS dados, FILE *arq)
         // int numberOfClusters = pattern;
         int numberOfClusters = 1;
         for (i = 0; i < numberOfClusters; i++)
-        {                                     //qntidade de clusters
-            for (j = 0; j < clusterSize; j++) // cada cluster
-            {
-                fwrite(&val, sizeof(char), 1, arq); //printa o valor zero de todos os clusters para determinar o tamanho m�x do arquivo
-            }
+        { //qntidade de clusters
+            apagaCluster(arq);
         }
     }
     fclose(arq); // fecha arquivo
@@ -90,12 +107,55 @@ void le_metadados(FILE *arq)
     fclose(arq); // fecha arquivo
 }
 
+void goToCluster(FILE *arq, int n)
+{
+    int x = n + 1;
+    int clusterDesloc = ((x * clusterSize) / 8);
+    clusterDesloc -= clusterSize / 8;
+    int desloc = clusterDesloc + rootPos;
+    //printf("%i \n", desloc);
+    //printf("desloc: %d \n", desloc);
+    fseek(arq, desloc, SEEK_SET); //deslocamento medido em bytes
+    printf("\0");
+    //printf("going to cluster %d \n", n);
+}
+
 // p = 0
 // a = 1
 
 // Perguntar:
 // - Cluster de continuação de pastas
 // - Cluster de continuação de arquivo
+void IniciaCluster(FILE *arq, int anterior, int novo)
+{
+    // marcar como ocupado na fat
+    goToCluster(arq, novo);
+
+    //. ..
+    fwrite(&novo, sizeof(int), 1, arq);
+    fwrite(&anterior, sizeof(int), 1, arq);
+    fseek(arq, (9 * sizeof(char) + (novo * sizeof(char))), SEEK_SET);
+
+    return;
+}
+
+int analisa_fat(FILE *arq)
+{
+    char preenchimento;
+    int indice = 1;
+
+    fseek(arq, (9 * sizeof(char)), SEEK_SET); // va para byte 9 -> inicio do indice +1 (pula o zero)
+
+    fread(&preenchimento, sizeof(char), 1, arq);
+
+    while (preenchimento != 0 && indice <= 254) //não vai para 255 pois este indica arquivo corrompido
+    {
+        fread(&preenchimento, sizeof(char), 1, arq);
+        indice++;
+    }
+
+    return indice;
+}
 
 ITEM *readFolderContent(FILE *arq)
 {
@@ -143,17 +203,6 @@ ITEM *readFolderContent(FILE *arq)
     return arr;
 }
 
-void goToCluster(FILE *arq, int n)
-{
-    int metadataDesloc = sizeof(METADADOS) - 1;
-    int fatTableDesloc = pattern;
-    int clusterDesloc = (n * clusterSize) / 8;
-    int desloc = metadataDesloc + fatTableDesloc + clusterDesloc;
-    //printf("desloc: %d \n", desloc);
-    fseek(arq, desloc, SEEK_SET); //deslocamento medido em bytes
-    //printf("going to cluster %d \n", n);
-}
-
 void writeItemInFolderCluster(FILE *arq, int cluster, ITEM item)
 {
     goToCluster(arq, cluster);
@@ -170,14 +219,16 @@ void writeItemInFolderCluster(FILE *arq, int cluster, ITEM item)
 
 void writeItemInCurrFolder(FILE *arq, ITEM *item)
 {
-    fseek(arq, sizeof(clmeta), SEEK_CUR);
     ITEM *i;
     bool t = 1;
-    int clusteratual = (ftell(arq) - 263) / clusterSize;
+    int clusteratual = (ftell(arq) - rootPos) / clusterSize;
+    fseek(arq, sizeof(clmeta), SEEK_CUR); // pula metadados
+    int fatpos = 0;
+    int posant = 0;
     //printf("cluster atual : %i \n", clusteratual);
 
     //printf("posição chegada: %d \n", ftell(arq));
-    while (t)
+    while (t) // Acha a primeira posição onde não tem um item escrito
     {
         fread(i, sizeof(ITEM), 1, arq);
         //printf("ler_bit: %d \n", i->ler_bit);
@@ -194,34 +245,94 @@ void writeItemInCurrFolder(FILE *arq, ITEM *item)
     //(*item).ponteiro = 11; // navegar a fat até achar local para criar a pasta/item
     //printf("escrita comecou na posicao: %d \n", ftell(arq));
 
-    fwrite(item, sizeof(ITEM), 1, arq);
+    // guarda posicao atual
+    posant = ftell(arq);
 
+    // acha cluster fat
+    fatpos = analisa_fat(arq);
+
+    item->ponteiro = fatpos;
+
+    // se eh pasta
     if (item->eh_pasta_bit)
     {
-        IniciaCluster(arq, clusteratual, i->ponteiro);
+        IniciaCluster(arq, clusteratual, fatpos);
+        fwrite(&item->ponteiro, sizeof(short), 1, arq); // escreve na fat que ela termina em si
+        fseek(arq, posant, SEEK_SET);
+        fwrite(item, sizeof(ITEM), 1, arq);
     }
+
+    // se eh arquivo
+    else
+    {
+        fseek(arq, (9 * sizeof(char) + (fatpos * sizeof(char))), SEEK_SET);
+        fwrite(&item->ponteiro, sizeof(short), 1, arq);
+        fseek(arq, posant, SEEK_SET);
+        fwrite(item, sizeof(ITEM), 1, arq);
+    }
+
+    goToCluster(arq, clusteratual);
 
     //printf("escrita terminou na posicao: %d \n", ftell(arq));
 }
 
 //printf("posição pos escrita: %d \n", ftell(arq));
+/*
+void CD(FILE *arq, char directoryPath[maxCharsInFileSystem])
+{
+    char *lastFolderNameOfPath = goToFolderAndReturnLastNameOfPath(arq, directoryPath);
+    ITEM *folderItems = readFolderContent(arq);
+    int x = folderItems[0].ler_bit;
+    int i = 0;
 
-/*}
-void CD(FILE *arq, char *comando)
-    /* ITEM *folderItems = readFolderContent(arq);/
-ra                                          //    eadfFldercCntent\()// }
+    while (x)
+    {
+        printf("%s ", folderItems[i].nome);
+        if (!strcmp(folderItems[i].nome, lastFolderNameOfPath))
+        {
+            goToCluster(arq, folderItems[i].ponteiro);
+            break;
+        }
+        i += 1;
+        x = folderItems[i].ler_bit;
+    }
+}
+
+char *goToFolderAndReturnLastNameOfPath(FILE *arq, char directoryPath[maxCharsInFileSystem])
+{
+    const char breakChar[1] = "/";
+
+    if (directoryPath[0] == breakChar)
+        goToCluster(arq, 0); // absolutePath
+
+    char *currentDir;
+    currentDir = strtok(directoryPath, breakChar);
+    while (currentDir != NULL)
+    {
+        char *nextDir = strtok(directoryPath, breakChar);
+
+        ITEM *folderItems = readFolderContent(arq);
+        int x = folderItems[0].ler_bit;
+        int i = 0;
+
+        while (x)
+        {
+            printf("%s ", folderItems[i].nome);
+            if (!strcmp(folderItems[i].nome, currentDir))
+            {
+                goToCluster(arq, folderItems[i].ponteiro);
+                break;
+            }
+            i += 1;
+            x = folderItems[i].ler_bit;
+        }
+
+        currentDir = nextDir;
+    }
+    return "oi";
+}
 
 */
-
-void IniciaCluster(FILE *arq, int anterior, int novo)
-{
-    goToCluster(arq, novo);
-    //. ..
-    fwrite(&novo, sizeof(int), 1, arq);
-    fwrite(&anterior, sizeof(int), 1, arq);
-
-    return;
-}
 
 void DIR(FILE *arq)
 {
@@ -249,9 +360,13 @@ void RM(FILE *arq, char *name)
     // Limpa a tabela FAT
     // Limpa a diretorio parente
 
+    int i;
+    short pAtual, pProx;
+
     ITEM *parentFolderItems = readFolderContent(arq); // Pega os itens do parente
     ITEM lixo;
-    
+    ITEM conteudo;
+
     i = 0 // Acha a posição do arquivo que deve ser removido
     while(strcmp(parentFolderItems[i].nome, name))
     {
@@ -267,60 +382,132 @@ void RM(FILE *arq, char *name)
     // }
     // i--;
     
-    fread(son, sizeof(ITEM), 1, arq);
+    fread(&lixo, sizeof(ITEM), 1, arq); // Le o item do arquivo que deve ser removido
+    goToCluster(arq, lixo.ponteiro); // Vai até o cluster que deve ser limpo 
     
+    if(lixo.eh_pasta_bit) // Testa se o item a ser removido é uma pasta
+    {
+        fseek(arq, sizeof(clmeta), SEEK_CUR) // Vai até o primeiro item da pasta
+        fread(&conteudo, sizeof(ITEM), 1, arq) // Le o primeiro item
+        
+        if(conteudo.ler_bit) // Testa se a pasta está vazia
+        {
+            // ### Avisa que a pasta não pode ser apagada porque ela ainda tem conteudo ###
+            return;
+        }
+        else
+        {
+            fseek(arq, -sizeof(clmeta) -sizeof(ITEM), SEEK_CUR); // Volta para o inicio da pasta
+            
+            apagaCluster(arq);
+        }
+    }
+    else // Caso seja um arquivo de texto
+    {
+        pAtual = lixo.ponteiro;
+        pProx = 0 // Valor inicial que nunca será igual ao pAtual (pois o 0 é o root);
+        while (pAnt != pProx) // Enquanto não chegar no end-of-file (ponteiro aponta para a posição atual)
+        {
+            pProx = readFat(arq, pAtual); // Acha qual é o próximo ponteiro
+            writeFat(arq,)
+            apagaCluster(arq); // Limpa o cluster atual
+            
+            gotoCluster(arq, pProx); // Vai para o próximo cluster
+        }
+    }
     
+
+    
+
     
 }
 */
 
 void MKDIR(FILE *arq, char *name)
 {
-    ITEM temp;
-    temp.ler_bit = 1;
-    temp.eh_pasta_bit = 1;
-    strcpy(temp.nome, name);
-
-    writeItemInCurrFolder(arq, &temp);
+    ITEM *temp;
+    temp = malloc(sizeof(ITEM));
+    temp->ler_bit = true;
+    temp->eh_pasta_bit = true;
+    strcpy(temp->nome, name);
+    printf("%s", temp->nome);
+    writeItemInCurrFolder(arq, temp);
 }
 
-// void MKFILE(FILE *arq, )
-// {
-//     ITEM *item;
-//     item = malloc(sizeof(ITEM));
-//     item->ler_bit = true;
-//     item->eh_pasta_bit = false;
-//     item->ponteiro = 2;
-//     strcpy(item->nome, "TESTE");
-//     writeItemInCurrFolder(arq, item);
-// }
-void EDIT()
+void MKFILE(FILE *arq, char fileName[16])
 {
+    ITEM *item;
+    item = malloc(sizeof(ITEM));
+    item->ler_bit = true;       //true
+    item->eh_pasta_bit = false; //false
+    item->ponteiro = 0;
+    strcpy(item->nome, fileName);
+    writeItemInCurrFolder(arq, item);
 }
 
-// void RENAME(FILE *arq, char *name, char *newname)
-// {
-//     int i = 0;
-//     ITEM item;
+void EDIT(FILE *arq, char fileDir[maxCharsInFileSystem], char fileContent[maxCharsInFileSystem])
+{
+    //encontrar cluster de acordo com diretorio
+    //-goToFolderAndReturnLastNameOfPath
+    //limpar cluster
+    //-apagaCluster
+    //gravar no diretorio
+    //-goToCluster(arq, 0);
+    //-while para quebrar arquivo em n clusters
+    //--procurar cluster disponivel fat
+    //--write
+    //--gravar sequenciamento fat
+}
 
-//     while (1)
-//     {
-//         fread(&item, sizeof(ITEM), 1, arq);
-//         if (item.ler_bit || (i > clusterSize / sizeof(ITEM)))
-//             break;
+void RENAME(FILE *arq, char *name, char *newname)
+{
+    int i = 0;
+    ITEM item;
 
-//         else
-//         {
-//             if (strcmp(&item.nome, name))
-//             {
-//                 strcpy(&item.nome, newname);
-//                 fseek(arq, -(sizeof(ITEM)), SEEK_CUR);
-//                 fwrite(&item, sizeof(ITEM), 1, arq);
-//             }
-//         }
+    while (1)
+    {
+        fread(&item, sizeof(ITEM), 1, arq);
+        if (!(item.ler_bit) || (i > clusterSize / sizeof(ITEM)))
+            break;
 
-// i += 1;
-// // }
+        else
+        {
+            if (!(strcmp(item.nome, name)))
+            {
+                strcpy(item.nome, newname);
+                fseek(arq, -(sizeof(ITEM)), SEEK_CUR);
+                fwrite(&item, sizeof(ITEM), 1, arq);
+            }
+
+            i += 1;
+        }
+    }
+}
+/*
+void MOVE(FILE *arq, char *name , char *name_destino, int clusteratual, int new_cluster ){
+// copiar arquivo
+// mover para novo diretorio
+// excluir do diretorio antigo 
+//atualizar fat
+
+    int i = 0;
+    ITEM item;
+
+    while (1)
+    {
+        fread(&item, sizeof(ITEM), 1, arq);
+        if (!(item.ler_bit) || (i > clusterSize / sizeof(ITEM)))
+            break;
+
+        else
+        {
+
+            i += 1;
+        }
+    }
+    
+}
+*/
 // FILE *arq, //
 //char *namet//   / /,void MOextVE() {}
 
@@ -334,23 +521,10 @@ void EDIT()
 //     }
 // }
 
-// retorna de 1 a 256
-int analisa_fat(FILE *arq)
+// retorna de 1 a 255
+void VivaAoC()
 {
-    char preenchimento;
-    int indice = 1;
-
-    fseek(arq, (8 * sizeof(char)), SEEK_SET); // va para byte 8 -> inicio do indice
-
-    fread(&preenchimento, sizeof(char), 1, arq);
-
-    while (preenchimento != 0)
-    {
-        fread(&preenchimento, sizeof(char), 1, arq);
-        indice++;
-    }
-
-    return indice;
+    printf("\0");
 }
 
 int main()
@@ -362,37 +536,39 @@ int main()
 
     arq = fopen("arquivo.bin", "r+");
 
+    // printf("teste \n");
+
     goToCluster(arq, 0);
-    printf("\n");
+
+    // printf("teste2");
+    // printf("teste2");
+
+    // printf("teste2");
+    // printf("AI");
 
     ITEM *item;
     item = malloc(sizeof(ITEM));
     item->ler_bit = true;
     item->eh_pasta_bit = true;
-    item->ponteiro = 2;
-    strcpy(item->nome, "TESTE");
-    writeItemInCurrFolder(arq, item);
-
-    item->ler_bit = true;
-    item->eh_pasta_bit = true;
     item->ponteiro = 33;
-    strcpy(item->nome, "Item123232");
+    strcpy(item->nome, "Item123");
+    MKDIR(arq, item->nome);
 
     goToCluster(arq, 0);
-    printf("\n");
 
-    writeItemInCurrFolder(arq, item);
+    strcpy(item->nome, "Item13222");
+    MKDIR(arq, item->nome);
 
     goToCluster(arq, 0);
-    printf("\n");
 
     DIR(arq);
 
-    printf("#4\n");
-
     // ITEM *folderItens = readFolderContent(arq);
     //printf("nome: %s \n", folderItens[0].nome);
-    printf("Fat: %d", analisa_fat(arq));
+    //printf("Fat: %d", analisa_fat(arq));
 
     fclose(arq);
 }
+
+// corrompido = 255
+//
